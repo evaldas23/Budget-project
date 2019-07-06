146c6b679d1b3087d08ffd43a8183bf0093ce243
Evaldas <evaldas.cerniauskas@gmail.com>
2019-07-06 23:17:19 +0200

    programing input fields

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 // BUDGET CONTROLLER
-var budgetController = (function(){
+var budgetController = (function () {
   //some code
 })();
 
 // UI CONTROLLER
-var UIController = (function(){
-  //some code
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    }
+  }
+
 })();
 
 //GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl){
+var controller = (function (budgetCtrl, UICtrl) {
 
-  var ctrlAddIthem = function(){
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddIthem = function () {
     // 1. Get the field input data
-
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
 
     // 3. Add the item to the UI
@@ -24,11 +46,12 @@ var controller = (function(budgetCtrl, UICtrl){
     console.log('Entered')
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddIthem)
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddIthem)
 
-  document.addEventListener('keypress', function(event){
-        if(event.keyCode === 13 || event.which === 13){
-          ctrlAddIthem();
-        }
-  })
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddIthem();
+    }
+  });
+
 })(budgetController, UIController);
